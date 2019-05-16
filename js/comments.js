e7be1375a90bbc5a57a56cb0ad0e50876357ce50
Joe Dolson <design@joedolson.com>
2019-05-16 14:01:11 -0500

    Remove focus assignments
    Assigning focus to an aria-live region causes double reading.

--- js/comments.js
+++ js/comments.js
@@ -2,33 +2,33 @@
  * Handle submission of comment form. Checks field values before submitting.
  */
 (function( $ ) { 'use strict';
-	$('document').ready( function( $ ) {	
+	$('document').ready( function( $ ) {
 		var commentForm = $('#commentform');
 		/**
 		 * Add aria-live attribute so message errors appended to input fields will be read by assistive technology.
-		 */			
+		 */
 		commentForm.attr( 'aria-live', 'polite' );
 		/**
 		 * Add a comment status region with role=status to provide feedback on errors or successes.
 		 * Set tabindex=-1 so that this container can receive focus.
 		 */
-		commentForm.prepend('<div id="comment-status" aria-live="assertive" role="status" tabindex="-1"></div>'); 
+		commentForm.prepend('<div id="comment-status" aria-live="assertive" role="status" tabindex="-1"></div>');
 		var statusdiv = $('#comment-status');
 		var list;
 		$('a.comment-reply-link').click( function() {
 			list = $(this).parents( '.comment' ).attr('id');
 		} );
-			
+
 		commentForm.submit(function(){
 			//serialize and store form data in a variable
 			var formdata = commentForm.serialize();
-			
+
 			$( '#commentform .aac-comment-error' ).remove();
 			$( '#commentform .aac-field-error' ).remove();
 			var hasError = false;
 			/**
 			 * Find all form fields with aria-required=true to test values before submitting.
-			 * If error generated, print error. 
+			 * If error generated, print error.
 			 */
 			$( '#commentform [aria-required=true]' ).each( function() {
 				var id = $(this).attr( 'id' ) + '-error';
@@ -49,23 +49,23 @@
 					}
 				}
 			});
-			
+
 			/**
 			 * If an error is generated, return without submitting the form & set focus to status div.
 			 */
 			if ( hasError ) {
-				statusdiv.html( '<p class="aac-comment-error">' + aac.error + '</p>' ).focus();
+				statusdiv.html( '<p class="aac-comment-error">' + aac.error + '</p>' );
 				return false;
-			}			
-			
-			/** 
+			}
+
+			/**
 			 * If no field errors, notify user that message is processing.
 			 */
 			statusdiv.html( '<p class="aac-comment-processing">' + aac.processing + '</p>' );
-			
+
 			//Extract action URL from commentform
 			var formurl = commentForm.attr('action');
-			
+
 			//Post Form with data
 			$.ajax({
 				type: 'post',
@@ -73,7 +73,7 @@
 				data: formdata,
 				dataType: 'json',
 				error: function( XMLHttpRequest, textStatus, errorThrown ) { // [localize]
-					statusdiv.html( '<p class="aac-comment-error">' + aac.flood + '</p>' ).focus();
+					statusdiv.html( '<p class="aac-comment-error">' + aac.flood + '</p>' );
 				},
 				success: function( data, textStatus ){
 					var success = data.success;
@@ -82,8 +82,8 @@
 					if ( success ) {
 						/**
 						 * Because the successful comment message includes a link, set focus to the message to give easy access to user.
-						 */						
-						statusdiv.html('<p class="aac-comment-success" >'+status+'</p>').focus();
+						 */
+						statusdiv.html('<p class="aac-comment-success" >'+status+'</p>');
 						//alert(data);
 
 						if ( $( '#comments' ).has( "ol.commentlist" ).length > 0 ) {
@@ -101,20 +101,20 @@
 						/**
 						 * Set focus to error field so user can quickly tab forward into comment fields.
 						 */
-						statusdiv.html('<p class="aac-comment-error" >'+status+'</p>').focus();
+						statusdiv.html('<p class="aac-comment-error" >'+status+'</p>');
 						commentForm.find('textarea[name=comment]').val('');
 					}
 				}
 			} );
 		return false;
 	});
-	
+
 	/**
 	 * Function to validate email format.
-	 * 
+	 *
 	 * @param value Email address to test
-	 * 
-	 * @return boolean 
+	 *
+	 * @return boolean
 	 */
 	function yourthemeValidateEmail( value ) {
 		var filter = /^([\w-\+.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
